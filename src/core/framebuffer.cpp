2643c448423503dd8845bfaa9827585f0a3c9690
Caleb Johnston <caleb.johnston+git@gmail.com>
2012-04-17 14:55:41 -0400

    mmm nothing.

--- src/core/framebuffer.cpp
+++ src/core/framebuffer.cpp
@@ -13,26 +13,29 @@ FrameBuffer::FrameBuffer(const unsigned width, const unsigned height, const unsi
 
 FrameBuffer::~FrameBuffer()
 {
-	allocateGLTexture();
+	this->deallocateGLTexture();
 	// free(this->m_cData);
 	// free(this->m_zData);
 }
 
 void FrameBuffer::init()
 {
+	/*
 	size_t c_len = 3 * m_width * m_height * sizeof(char);
 	size_t z_len = m_width * m_height * sizeof(float);
 	m_cData = (char*) malloc(c_len);
 	m_zData = (float*) malloc(z_len);
 	memset(m_cData, 0, c_len);
 	memset(m_zData, 0, z_len);
+	*/
 	
 	allocateGLTexture();
 }
 
 float FrameBuffer::getZ(const int x, const int y) const
 {
-	return this->m_zData[x + this->m_width * y];
+	// return this->m_zData[x + this->m_width * y];
+	return this->m_raster->head() + (y * this->m_raster->width() + x)*this->m_raster->channels();
 }
 
 void FrameBuffer::set(int ix, int iy, float r, float g, float b, float z)
@@ -76,17 +79,13 @@ void FrameBuffer::allocateGLTexture()
 		this->m_bAllocated = true;
 	}
 }
-void FrameBuffer::allocateGLTexture()
+void FrameBuffer::deallocateGLTexture()
 {	
 	if(!this->m_bAllocated){
-		glGenTextures(1, &(this->m_textureHandle));
-		glBindTexture(GL_TEXTURE_2D, this->m_textureHandle);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		this->m_bAllocated = true;
+		glDeleteTextures(1, &(this->m_textureHandle));	//???
+		this->m_bAllocated = false;
 	}
 }
-void deallocateGLTexture();
 
 void FrameBuffer::drawGLTexture(float x, float y) const 
 {
